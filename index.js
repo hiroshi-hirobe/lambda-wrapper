b414c69d970b2d3a0dda9e353944284f94e4838c
hiroshi-hirobe <hiroshi.hirobe@esm.co.jp>
2024-11-22 17:38:50 +0900

    aws-sdk v2 -> @aws-sdk/client-lambda (aws-sdk v3)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const AWS = require('aws-sdk');
+const Lambda = require("@aws-sdk/client-lambda");
 
 // Wrapper class for AWS Lambda
 class Wrapped {
@@ -8,23 +8,24 @@ class Wrapped {
     const options = opts || {};
 
     this.lambdaModule = mod;
-    const handler = options.handler || 'handler';
+    const handler = options.handler || "handler";
 
     if (mod[handler]) {
       this.handler = mod[handler];
     } else {
       if (!this.lambdaModule.lambdaFunction) {
-        throw new Error("Invalid input. Expected either lambda module (with exposed handler) " +
-          "or AWS Lambda configuration (with provided 'lambdaFunction' name)");
+        throw new Error(
+          "Invalid input. Expected either lambda module (with exposed handler) " +
+            "or AWS Lambda configuration (with provided 'lambdaFunction' name)"
+        );
       }
     }
   }
 
   runHandler(event, customContext, cb) {
     return new Promise((resolve, reject) => {
-
       const promiseCallback = (error, response) => {
-        if(error) {
+        if (error) {
           return reject(error);
         }
         return resolve(response);
@@ -33,54 +34,61 @@ class Wrapped {
       const callback = cb || promiseCallback;
 
       const defaultContext = {
-        succeed: success => callback(null, success),
-        fail: error => callback(error, null),
-        done: (error, success) => callback(error, success)
+        succeed: (success) => callback(null, success),
+        fail: (error) => callback(error, null),
+        done: (error, success) => callback(error, success),
       };
 
       const lambdaContext = Object.assign({}, defaultContext, customContext);
 
       try {
-        if (this.handler) { 
+        if (this.handler) {
           const handlerResult = this.handler(event, lambdaContext, callback);
           // Check if the result itself is a promise
           if (handlerResult && handlerResult.then) {
-            handlerResult.then(function(data) {
-              // Avoid Maximum call stack size exceeded exceptions
-              return setImmediate(function() {
-                callback(null, data);
+            handlerResult
+              .then(function (data) {
+                // Avoid Maximum call stack size exceeded exceptions
+                return setImmediate(function () {
+                  callback(null, data);
+                });
+              })
+              .catch(function (err) {
+                // Avoid Maximum call stack size exceeded exceptions
+                return setImmediate(function () {
+                  callback(err);
+                });
               });
-            }).catch(function(err) {
-               // Avoid Maximum call stack size exceeded exceptions
-               return setImmediate(function() {
-                callback(err);
-              });
-            });
           }
         } else {
-          if (this.lambdaModule.region) {
-            AWS.config.update({
-              region: this.lambdaModule.region
-            });
-          }
+          const lambda = this.lambdaModule.region
+            ? new Lambda.LambdaClient({ region: this.lambdaModule.region })
+            : new Lambda.LambdaClient();
 
-          const lambda = new AWS.Lambda();
           const params = {
             FunctionName: this.lambdaModule.lambdaFunction,
-            InvocationType: 'RequestResponse',
-            LogType: 'None',
-            Payload: JSON.stringify(event)
+            InvocationType: "RequestResponse",
+            LogType: "None",
+            Payload: JSON.stringify(event),
           };
+          const command = new Lambda.InvokeCommand(params);
 
-          lambda.invoke(params, (err, data) => {
-            if (err) {
+          lambda
+            .send(command)
+            .then(function (data) {
+              if (data.FunctionError) {
+                return callback(
+                  Object.assign(
+                    new Error(JSON.parse(data.Payload).errorMessage),
+                    data
+                  )
+                );
+              }
+              return callback(null, JSON.parse(data.Payload));
+            })
+            .catch(function (err) {
               return callback(err);
-            }
-            if (data.FunctionError) {
-              return callback(Object.assign(new Error(JSON.parse(data.Payload).errorMessage), data));
-            }
-            return callback(null, JSON.parse(data.Payload));
-          });
+            });
         }
       } catch (ex) {
         return callback(ex);
@@ -91,7 +99,7 @@ class Wrapped {
   run(event, context, callback) {
     let callbackFunction = callback;
     let contextObject = context;
-    if (typeof context === 'function') {
+    if (typeof context === "function") {
       // backwards compability
       callbackFunction = context;
       contextObject = {};
@@ -111,7 +119,6 @@ let latest;
 // Public interface for the module
 
 module.exports = exports = {
-
   // reusable wrap method
   wrap,
 
@@ -119,27 +126,28 @@ module.exports = exports = {
   init: (mod, options) => {
     latest = wrap(mod, options);
   },
-  run: (event, context, callback) => new Promise((resolve, reject) => {
-    let callbackFunction = callback;
-    let contextObject = context;
-    if (typeof context === 'function') {
-      // backwards compability
-      callbackFunction = context;
-      contextObject = {};
-    }
-    if (typeof latest === typeof undefined) {
-      const error = 'Module not initialized';
-      reject(error);
-      return callbackFunction(error, null);
-    }
-    return latest.run(event, contextObject, (err, data) => {
-      if (callbackFunction) {
-        return callbackFunction(err, data);
+  run: (event, context, callback) =>
+    new Promise((resolve, reject) => {
+      let callbackFunction = callback;
+      let contextObject = context;
+      if (typeof context === "function") {
+        // backwards compability
+        callbackFunction = context;
+        contextObject = {};
       }
-      if (err) {
-        return reject(err);
+      if (typeof latest === typeof undefined) {
+        const error = "Module not initialized";
+        reject(error);
+        return callbackFunction(error, null);
       }
-      return resolve(data);
-    });
-  })
+      return latest.run(event, contextObject, (err, data) => {
+        if (callbackFunction) {
+          return callbackFunction(err, data);
+        }
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    }),
 };
